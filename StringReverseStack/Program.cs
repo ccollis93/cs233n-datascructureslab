d5ce8dc18e474c6b611cb2917338b47dd5a2b77f
ccollis93 <63563783+ccollis93@users.noreply.github.com>
2023-01-03 19:56:30 -0800

    Add files via upload

---
+++ StringReverseStack/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverseStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stackChar = new Stack<char>();
+            Console.WriteLine("Please enter a 5 letter word to push into the stack");
+            for (int i = 0; i < 5; i++)
+            {
+                char n = char.Parse(Console.ReadLine());
+                
+                stackChar.Push(n);
+               
+            }
+            Console.WriteLine("the stack reversed is: ");
+            int count = stackChar.Count;
+            for (int i = 0; i < count; i++)
+            {
+                char n = stackChar.Pop();
+                Console.Write(n + " ");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
